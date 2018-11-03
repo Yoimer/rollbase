2fe41868de2c943f5aaab23dc8115be72c7996ad
Yoimer <ydrf19@gmail.com>
2018-11-03 11:14:04 -0400

    added show_google_maps_link_on_location_new.js for showing or hidden google maps link when creating a new location

---
+++ show_google_maps_link_on_location_new.js
@@ -0,0 +1,33 @@
+<script>
+    customOnPageRender(function() {
+
+        function processFromGpsInfo()
+        {
+
+            console.log("On processFromGpsInfo!!!");
+            // if Gps_coordinates text input field is NOT empty
+            // if($("#Gps_coordinates").val() !== "") {
+                if(document.getElementById("Gps_coordinates").value !== "") {
+                //shows google class
+                document.getElementsByClassName('google')[0].style.display = "block";
+                // var GpsFrom = $("#Gps_coordinates").val();
+                var GpsFrom = document.getElementById("Gps_coordinates").value;
+                let googlemap = 'https://www.google.com/maps/@?api=1&map_action=map&center=' + GpsFrom + '&zoom=12'
+                // sets link as href
+                document.querySelector('#the-link').setAttribute('href', googlemap);
+            // if Gps_coordinates text input field is empty
+            }else {
+                // hides google class
+                document.getElementsByClassName('google')[0].style.display = "none";
+            }
+
+        }
+
+        // go to processFromGpsInfo when any change in Gps_coordinates text input field
+        document.getElementById("Gps_coordinates").addEventListener("change",processFromGpsInfo);
+        document.getElementById("Gps_coordinates").addEventListener("keyup",processFromGpsInfo);
+
+        // hides google class
+        document.getElementsByClassName('google')[0].style.display = "none";
+    });
+</script>
