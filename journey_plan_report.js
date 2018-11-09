ea742fe4d7a9cca7673d343485a2254f8b5fb0b6
Yoimer <ydrf19@gmail.com>
2018-11-09 08:57:37 -0400

    added journey_plan_report.js

---
+++ journey_plan_report.js
@@ -0,0 +1,46 @@
+<!-- ==================================================        -->
+<!-- Page Name..: Job's information Report                     -->
+<!-- Application: LongView Plus                                -->
+<!-- Object.....: LongView/Journey Plan                        -->
+<!-- Purpose....: Template for Journey Plan information report -->
+<!-- Date.......: 08/07/2018                                   -->
+<!-- Programmer.: Nextwells delevopment team                   -->
+<!-- ==================================================        -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Journey Plan Report {!R1671927#text} </title>
+ <style>
+td 
+{
+border: 1px solid black;
+font-size :10px;
+}
+h5
+{
+font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+font-size :10px;    
+}
+
+</style>
+</head>
+<br>
+<body>
+<!-- start -->
+<table style="border: 0px solid black;" width="100%" bgcolor="#7BD2F6"> 
+    <tr> 
+    <td width="60%" style="border: 0px solid black;"><img src="http://castillolk.com.ve/ray-oil-gas.png" width=20% ></td>
+    <td  style="border: 0px solid black;"><div align=center> <b>Ray International Oil & gas<br>Daily Operational Report <br>
+<!--     <font color=red>{!Job_Number_no_hyperlink}</font></b>  -->
+    <font color=red></font></b>
+    </div></td>
+    </tr>
+</table>
+<br>
+<div style="background:#d5d5d5;" align=center>  
+<table  style="border: 0px solid black;"width = "100%"><tr><td align=center> 
+<h5>Journey Plan Information</h5><br> 
+</td></tr></table>
+</div>
