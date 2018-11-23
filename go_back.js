60c082cd3c7bb7c9325428a0f5e531a9bd29b408
Yoimer <ydrf19@gmail.com>
2018-11-23 10:41:51 -0400

    added go_back function

---
+++ go_back.js
@@ -0,0 +1,22 @@
+<script src="{!#HOSTED_FILE.qPTNhfHDRFKcFRvedTvToA}">
+//Hide ALL View Page Action buttons Except: Tag, Send and Delete
+</script>
+
+<script>
+//Change New Page icons for Text
+ {!#HOSTED_FILE.4BKYthbxSpCCv2vamOCc8g#text} //Notice this has te text tag
+  
+  //Hide Save and New Button
+ {!#HOSTED_FILE.40Bb0fbGS_mLxXbKUZUfDA#text}
+
+</script>
+
+  <script>
+  	{!#HOSTED_FILE.jOVXdNWIRdesSapNiNbKmQ#text}  //Enable Back to List button
+	customOnPageRender(function () {
+     enableBackButton("{!#LINK.Equipment1#rPOrhQ_5RRW3bQcqUppRoQ}"); // Equipment Location
+});
+</script>
+
+<!-- //hide next prev buttons -->
+{!#HOSTED_FILE.G-bRxbgoSg6F4Tx4EWgFDQ#text} 
