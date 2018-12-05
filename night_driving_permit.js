46298be2af5354c3d9a50f50c8cc333d1429cd75
Yoimer <ydrf19@gmail.com>
2018-12-05 12:30:02 -0400

    added night_driving_permit.js

---
+++ night_driving_permit.js
@@ -0,0 +1,17 @@
+<script>
+    // every time NIGHT driving permit changes, checks if YES or NO was selected
+    $("#Night_driving_permit").on("change", function() {
+        let drop_down_value = document.getElementById("Night_driving_permit");
+        if((drop_down_value.options[drop_down_value.selectedIndex].text) === "YES") {
+            // check when Authorizing person for ND is empty
+            if(document.getElementById("Authorizing_person_for_ND").value === "") {
+                alert("Authorizing person for ND can not be empty, please press OK and fill this field")
+            }
+        } else if ((drop_down_value.options[drop_down_value.selectedIndex].text) === "NO") {
+            //fill Authorizing person for ND field
+            document.getElementById("Authorizing_person_for_ND").value = "NO Night Driving";
+        }
+    });
+</script>
+
+
