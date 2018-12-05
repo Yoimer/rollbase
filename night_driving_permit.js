65dfcbca8442be17c1b6889f8c133c363e4ca3a3
Yoimer <ydrf19@gmail.com>
2018-12-05 13:41:25 -0400

    cleans Authorizing_person_for_ND when selecting yes and it was previously no

--- night_driving_permit.js
+++ night_driving_permit.js
@@ -6,6 +6,9 @@
             // check when Authorizing person for ND is empty
             if(document.getElementById("Authorizing_person_for_ND").value === "") {
                 alert("Authorizing person for ND can not be empty, please press OK and fill this field")
+            } else if (document.getElementById("Authorizing_person_for_ND").value === "NO Night Driving") {
+                //alert("No driving");
+                document.getElementById("Authorizing_person_for_ND").value = "";
             }
         } else if ((drop_down_value.options[drop_down_value.selectedIndex].text) === "NO") {
             //fill Authorizing person for ND field
