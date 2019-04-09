87fd7546b7a46c3f26872c0c8fe2a19d2a2e4ef5
Yoimer <ydrf19@gmail.com>
2019-04-09 10:48:04 -0430

    update on Mrs Carme's code for api personnel competency

--- record_in_api_personnel_competency.js
+++ record_in_api_personnel_competency.js
@@ -1,41 +1,41 @@
-//If new or modification of a worker's competence occurs, the change in API Personnel Competency
+//If new of a worker's competence occurs, the change in API Personnel Competency
 // is notified.
 
 var arr = [];
-var currentDate = new Date(rbv_api.getCurrentDate());
+var ActualDate = new Date(rbv_api.getCurrentDate());
 // When making a change in the competence of a worker you have the identification of the worker through
 // the relationship between Personnel and Personnel Competency
 
 var CompetencyID =  "{!R148251#value}"; 
- 
-var personnelName = rbv_api.getRelatedFields("R148244","{!id}", "name");
 
-var employeeNo = rbv_api.selectQuery("SELECT id FROM API_Personnel WHERE name#value='" + personnelName[0] + "'", 1);
-rbv_api.println(employeeNo.length);
-rbv_api.println("prueba");
 
-if (employeeNo.length > 0){
-    arr.personnelName       = "{!name#value}";
-    arr.Date_Transfer       = "currentDate";
-    arr.Expiration_Date     = "{!Expiration_Date}";
-    arr.R6738299            = [employeeNo[0][0]]; //API Personnel - APi Personnel Competency relationship
+var employeeNo = rbv_api.getRelatedFields("R148244","{!id}", "Employee_Number");
+
+rbv_api.println ("employeeNo " + employeeNo[0]);
+// Seleccionar el ID del API Personnel para asignarle el valor a la relación R6739733 y asi establecer
+// la relación entre API Personnel y API Personnel Competency
+var employeeID = rbv_api.selectQuery("SELECT id FROM API_Personnel WHERE name#value='" + employeeNo[0] + "'", 1);
+
+rbv_api.println ("longitud " + employeeID.length);
+
+if (employeeID.length > 0){
+    arr.Competency_Code     = CompetencyID; 
+    arr.name        = "{!name#value}";
+    arr.Date_Transfer       = ActualDate;
+    arr.Expiration_Date     = "{!Expiration_Date_Expression}";
+    arr.R6739733 = [employeeID[0][0]]; //API Personnel - APi Personnel Competency relationship
     arr.Operation           = "I";
-    arr.Sent                = "True"; 
-        
-  if (rbv_api.isUpdate()) {
-    
-    rbv_api.println("update personnel competency");
-    
-    var PersonnelCompeId = rbv_api.selectQuery("SELECT id FROM  API_Personnel_Competency WHERE Competency_Code  = '" + CompetencyID + "'", 1);
-    
-    if (PersonnelCompeId.length > 0) {
-        arr.Operation           = "U";
-        rbv_api.updateRecord("API_Personnel_Competency", PersonnelCompeId[0][0], arr);
-    }
-  } else {
-    
-    rbv_api.println("create personnel competency");
+    arr.Sent                = "1"; 
+
+    rbv_api.println ("arr.Competency_Code " + arr.Competency_Code);
+    rbv_api.println ("arr.name " + arr.name);
+    rbv_api.println ("arr.Date_Transfer " + arr.Date_Transfer);
+    rbv_api.println ("arr.Expiration_Date " + arr.Expiration_Date); 
+    rbv_api.println ("arr.R6739733 " + arr.R6739733);
+    rbv_api.println ("arr.Operation " + arr.Operation);
+    rbv_api.println ("arr.Sent " + arr.Sent);
+
     rbv_api.createRecord("API_Personnel_Competency", arr);
+    
+    rbv_api.println ("create personnel competency ");
   }
-  
-}
