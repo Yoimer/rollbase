5b3da878fa9b5c9df15199f7eae4e0c94335d786
Yoimer <ydrf19@gmail.com>
2019-04-11 18:17:28 -0430

    added scripts folder that contains file about how to start and stop rollbase server automatically

---
+++ scripts/startdb-tcman.bat
@@ -0,0 +1,3 @@
+@echo off 
+REM does not show anything on screen and start tcman.bat script C:\Progress\Rollbase\Pas_Instance\bin\tcman.bat
+cmd /k "C:\Progress\Rollbase\Pas_Instance\bin\tcman.bat start"
---
+++ scripts/stopdb-rollbase.bat
@@ -0,0 +1,3 @@
+@echo off 
+REM does not show anything on screen, moves to C:\Progress\WRK\oe_rollbase_db folder and start stopdb.bat script
+cmd /k call "C:\Progress\WRK\oe_rollbase_db\stopdb.bat" 
