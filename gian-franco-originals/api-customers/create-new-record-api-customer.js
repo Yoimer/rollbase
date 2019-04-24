f522fe2fc5f23e18c96b969bc5a9ef15eebb2b7b
Yoimer <ydrf19@gmail.com>
2019-04-24 12:54:17 -0430

    updated api-customers by filling date transfer, operation and sent fields

--- gian-franco-originals/api-customers/create-new-record-api-customer.js
+++ gian-franco-originals/api-customers/create-new-record-api-customer.js
@@ -2,6 +2,8 @@ var arr = new Array();
 
 var customerName =  "{!name#value}";
 
+var ActualDate = new Date(rbv_api.getCurrentDate());
+
 var customerId = rbv_api.selectQuery("SELECT id FROM API_Customer WHERE name#value= '"+customerName+"'",1);
 
 arr["Client_Address"]    = "{!streetAddr1#value}";
@@ -9,20 +11,22 @@ arr["Client_Address1"]   = "{!streetAddr2#value}";
 arr["Client_Email"]      = "{!email}";
 arr["Client_Name"]       = "{!Customer_Name#value}";
 arr["name"]              = "{!name}";
+arr["Date_Transfer"]     = ActualDate;
+arr["Sent"]              = "True";
 
-if (rbv_api.isUpdate()){
-  
-  rbv_api.println("Customer Update");
-  
-  if (customerId.length > 0){
-    
-    rbv_api.updateRecord("API_Customer",customerId[0][0], arr);
-  }
-    
-}else{
-  
-  rbv_api.println("Customer Create");
-  
-  rbv_api.createRecord("API_Customer", arr);
-  
+// if updates record
+if (rbv_api.isUpdate()) {
+
+    arr["Operation"] = "U";
+    rbv_api.println("Customer Update");
+
+    if (customerId.length > 0){
+        rbv_api.updateRecord("API_Customer",customerId[0][0], arr);
+    }
+
+//if creates records
+} else if(rbv_api.isCreate()) {
+    arr["Operation"] = "I";
+    rbv_api.println("Customer Create");
+    rbv_api.createRecord("API_Customer", arr);
 }
