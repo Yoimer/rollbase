824c059327473276c19a8793fc2bc87e4e9b9133
Yoimer <ydrf19@gmail.com>
2019-04-24 17:49:27 -0430

    updated api-contracts after modifying trigger

--- gian-franco-originals/api-contracts/create-new-record-api-contract.js
+++ gian-franco-originals/api-contracts/create-new-record-api-contract.js
@@ -1,5 +1,6 @@
 var arr = new Array();
 
+var ActualDate = new Date(rbv_api.getCurrentDate());
 
 //var customerName = "{!R124988.name#value}";
 var contractName = "{!Contract_Name#value}";
@@ -11,28 +12,34 @@ var customerName = rbv_api.getRelatedFields("R124988","{!id}", "name");
 var apiCustomerId = rbv_api.selectQuery("SELECT id FROM API_Customer WHERE name#value='" + customerName[0] + "'", 1);
 
 //si el registro del cliente existe en el objeto API_CUSTOMER
-if (apiCustomerId.length > 0){
-  
-  arr["name"] = "{!name#value}";
-  arr["Contract_Detail"] = "{!Description}";
-  arr["Contract_Name"] = "{!Contract_Name}";
-  arr["R6736381"] = [apiCustomerId[0][0]]; //API Customer - APi Contract relationship
-  arr["Valid_From"] =new Date("{!Starting_Date}");
-  arr["Valid_To"] = new Date("{!Expiration_Date}");
-  
-  if (rbv_api.isUpdate()) {
-    
-    rbv_api.println("update contract");
-    
-    var contractId = rbv_api.selectQuery("SELECT id FROM API_Contract WHERE Contract_Name= '" + contractName + "'", 1);
-    
-    if (contractId.length > 0) {
-      rbv_api.updateRecord("API_Contract", contractId[0][0], arr);
+if (apiCustomerId.length > 0) {
+
+    arr["name"] = "{!name#value}";
+    arr["Contract_Detail"] = "{!Description}";
+    arr["Contract_Name"] = "{!Contract_Name}";
+    arr["R6736381"] = [apiCustomerId[0][0]]; //API Customer - APi Contract relationship
+    arr["Valid_From"] =new Date("{!Starting_Date}");
+    arr["Valid_To"] = new Date("{!Expiration_Date}");
+    arr["Date_Transfer"] = ActualDate;
+    arr["Sent"] = "True";
+
+    // if updates record
+    if (rbv_api.isUpdate()) {
+
+        arr["Operation"] = "U";
+        rbv_api.println("update contract");
+        var contractId = rbv_api.selectQuery("SELECT id FROM API_Contract WHERE Contract_Name= '" + contractName + "'", 1);
+
+
+        if (contractId.length > 0) {
+            rbv_api.updateRecord("API_Contract", contractId[0][0], arr);
+        }
+
+      // if creates record
+    } else if (rbv_api.isCreate()){
+
+          arr["Operation"] = "I";
+          rbv_api.println("create contract");
+          rbv_api.createRecord("API_Contract", arr);
     }
-  } else {
-    
-    rbv_api.println("create contract");
-    rbv_api.createRecord("API_Contract", arr);
-  }
-  
 }
