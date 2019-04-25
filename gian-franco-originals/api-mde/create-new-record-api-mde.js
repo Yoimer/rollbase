efd09f267f83baa5b96b8761e070757f41d866f2
Yoimer <ydrf19@gmail.com>
2019-04-25 09:31:37 -0430

    updated api-mde after modifying trigger and view

--- gian-franco-originals/api-mde/create-new-record-api-mde.js
+++ gian-franco-originals/api-mde/create-new-record-api-mde.js
@@ -2,18 +2,26 @@ var arr = new Array();
 
 var mdeName = "{!name#value}";
 
+var ActualDate = new Date(rbv_api.getCurrentDate());
+
 var mdeId = rbv_api.selectQuery("SELECT id FROM API_MDE WHERE name#value= '" + mdeName + "'", 1);
 
 arr["name"] = "{!name#value}";
 arr["Short_Description"] = "{!Description}";
+arr["Date_Transfer"] = ActualDate;
+arr["Sent"] = "True";
 
+// if updates record
 if (rbv_api.isUpdate()) {
 
     if (mdeId.length > 0) {
+        arr["Operation"] = "U";
         rbv_api.updateRecord("API_MDE", mdeId[0][0], arr);
     }
 
-} else {
+// if creates record
+} else if (rbv_api.isCreate()){
 
+    arr["Operation"] = "I";
     rbv_api.createRecord("API_MDE", arr);
 }
