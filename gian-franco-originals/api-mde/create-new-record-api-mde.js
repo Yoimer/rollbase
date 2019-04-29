11c8c7793efe07aa2eaa2e6ecae73bc29406b4b4
Yoimer <ydrf19@gmail.com>
2019-04-29 09:40:58 -0430

    updated api mde in sos server

--- gian-franco-originals/api-mde/create-new-record-api-mde.js
+++ gian-franco-originals/api-mde/create-new-record-api-mde.js
@@ -4,10 +4,10 @@ var mdeName = "{!name#value}";
 
 var ActualDate = new Date(rbv_api.getCurrentDate());
 
-var mdeId = rbv_api.selectQuery("SELECT id FROM API_MDE WHERE name#value= '" + mdeName + "'", 1);
+var mdeId = rbv_api.selectQuery("SELECT id FROM API_MDE1 WHERE name#value= '" + mdeName + "'", 1);
 
 arr["name"] = "{!name#value}";
-arr["Short_Description"] = "{!Description}";
+arr["Short_Description"] = "{!Short_Description_Text}";
 arr["Date_Transfer"] = ActualDate;
 arr["Sent"] = "True";
 
@@ -16,12 +16,12 @@ if (rbv_api.isUpdate()) {
 
     if (mdeId.length > 0) {
         arr["Operation"] = "U";
-        rbv_api.updateRecord("API_MDE", mdeId[0][0], arr);
+        rbv_api.updateRecord("API_MDE1", mdeId[0][0], arr);
     }
 
 // if creates record
 } else if (rbv_api.isCreate()){
 
     arr["Operation"] = "I";
-    rbv_api.createRecord("API_MDE", arr);
+    rbv_api.createRecord("API_MDE1", arr);
 }
