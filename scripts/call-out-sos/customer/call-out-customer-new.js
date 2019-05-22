0ce208ceea8b1a5478e7e67f33c961240f7388f9
Yoimer <ydrf19@gmail.com>
2019-05-22 12:35:55 -0400

    updated call-out-customer-new

--- scripts/call-out-sos/customer/call-out-customer-new.js
+++ scripts/call-out-sos/customer/call-out-customer-new.js
@@ -1,29 +1,37 @@
 <script>
-/ -------------------------------------------------------------------------------------------------- 
+/* -------------------------------------------------------------------------------------------------- 
 As soon as user selects customer, LongView shows all the information for it. 
 
 NOTE: This script uses HTML Components as fictitious fields for Customer information.
--------------------------------------------------------------------------------------------------- /
+-------------------------------------------------------------------------------------------------- */
 customOnPageRender(function() {
 
-    // Shows related From customer information.
+    // shows information from customer object
     function getsFromCustomerInformation(objName, objId, values)
     { 
-        $("#customercontact").val(values["Contact_Name"]); 
-        $("#companyname").val(values["Customer_Name"]); 
+        $("#CompanyName").val(values["Customer_Name"]);
+        $("#PhoneNumber").val(values["phone"]);
+        $("#ContactEmail").val(values["email"]);
+        $("#ContactName").val(values["Contact_Name"]);
     }
 
     function processcustomerInfo()
     {
+        console.log("on processcustomerInfo!!! ");
         var id = $("#R122034").val(); // customer Relationship Name.
         if (id == null) {
             $("#R122034").val('');
+            $("#CompanyName").val('');
+            $("#PhoneNumber").val('');
+            $("#ContactEmail").val('');
+            $("#ContactName").val('');
         }
         else {
-            rbf_getFields("LongviewCustomer",id,"Contact_Name,Customer_Name", getsFromCustomerInformation);
+            rbf_getFields("LongviewCustomer",id,"Customer_Name,phone,email,Contact_Name", getsFromCustomerInformation);
         }
     }
 
+    // if there is any change on customer field, and customer field is not empty, go to processcustomerInfo
     $("#R122034").change(processcustomerInfo);
     if($("#R122034").val() !== "") processcustomerInfo();
 });
