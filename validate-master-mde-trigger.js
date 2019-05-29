edae5a458c30c316358f6b50291ab653cb61e968
Yoimer <ydrf19@gmail.com>
2019-05-29 09:59:05 -0400

    added each master mde from service validation with MasterName on validate-master-mde-trigger.js

--- validate-master-mde-trigger.js
+++ validate-master-mde-trigger.js
@@ -16,7 +16,7 @@ var existsMPMDE     = rbv_api.selectQuery("SELECT id  FROM  Maintenance_Plan WHE
 var existsMasterMDEFromService = rbv_api.getRelatedIds("R2157847", '{!id}');
 rbv_api.println("existsMasterMDEFromService  "+ existsMasterMDEFromService.length);
 
-// if there are any services, gets its Master MDE from it
+// if there are any services, gets its Master MDE from it and parses it
 for (var i = 0; i < existsMasterMDEFromService.length; ++i) {
 
     // prints services ids
@@ -29,7 +29,28 @@ for (var i = 0; i < existsMasterMDEFromService.length; ++i) {
     rbv_api.println("MasterMDEFromService Length: "+ MasterMDEFromService.length);
 
     for (var j = 0; j < MasterMDEFromService.length; ++j) {
-        rbv_api.println(MasterMDEFromService[i][j]);
+
+        // prints Master MDE from Service
+        rbv_api.println("Master MDE from Service: " + MasterMDEFromService[i][j]);
+
+        // saves each master mde from service in a 1x1 array, it comes in a comma separated format
+        var EachMasterMDEFromService = MasterMDEFromService[i][j].split(", ");
+
+        // this prints all each master mde from service individually
+
+        // for(var w = 0; w < EachMasterMDEFromService.length; ++w) {
+        //    rbv_api.println("EachMasterMDEFromService: " + EachMasterMDEFromService[w]);
+        // }
+
+        // checks if any of the master mde from service matches with MasterName
+        // if it does, returns: MasterName+" has "+ existsMasterMDEFromService.length +  " Services attached";
+
+        for(var w = 0; w < EachMasterMDEFromService.length; ++w) {
+            if(EachMasterMDEFromService[w] == MasterName) {
+                return MasterName + " has "+ existsMasterMDEFromService.length + " Services attached";
+            }
+        }
+
     }
 
 }
