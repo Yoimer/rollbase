35bb3d3c5dd07a39e9b5ab09bb856ee89d3fa017
Yoimer <ydrf19@gmail.com>
2019-06-21 11:24:20 -0430

    updated clone-call-out-when-draft on ray, forces date of request in order to avoid validation error

--- triggers/ray/clone-call-out-when-draft.js
+++ triggers/ray/clone-call-out-when-draft.js
@@ -2,5 +2,10 @@
 // create new entry on call out db cloning all the previous fields
 
 var arr = new Array();
+
+// forces date of request value
+// this avoids error on date of request validation
+arr["Date_of_Request"] = new Date("{!Start_Date}");
+
 // clone record 
 rbv_api.cloneRecord("Call_Out", {!id}, arr);
