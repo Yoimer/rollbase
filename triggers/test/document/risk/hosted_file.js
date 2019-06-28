cfe785ab980a2bccda8fd4e1647637e4f1892c4a
Yoimer <ydrf19@gmail.com>
2019-06-28 17:19:29 -0430

    set isLocked to false in document/risk/hosted_file.js

--- triggers/test/document/risk/hosted_file.js
+++ triggers/test/document/risk/hosted_file.js
@@ -172,7 +172,7 @@ var cloneAndAttachRisk = function cloneAndAttachRisk(obj) {
       Risk_Hazard: attached[0][1],
       status: "APPROVED",
 
-       isLocked: true //Lock the object
+       isLocked: false //Lock the object
     };
     //sendDebugData(`This is the new risk to create: ${JSON.stringify(risk)}`)
 
