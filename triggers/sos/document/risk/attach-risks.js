705d81223b2f0c5c264c59af29c4e8b081eff9e0
Yoimer <ydrf19@gmail.com>
2019-07-03 16:48:58 -0430

    added risk folder using attach_risk and its hosted_file in sos

---
+++ triggers/sos/document/risk/attach-risks.js
@@ -0,0 +1,13 @@
+{!#HOSTED_FILE.vVLSzcVhTUibHUC83GJkxg#text}
+
+var object = {
+  relationshipName: "R121561",
+  name: "Procedure",
+  id: "{!id}",
+  previous: "{!Previous_Risks}",
+  after: "{!R121561}",
+  previousIntegrationName: "Previous_Risks",
+  relatedObjectName: "Risk"
+};
+
+cloneAndAttachRisk(object);
