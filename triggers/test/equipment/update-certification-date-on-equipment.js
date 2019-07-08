1ab1a595edd2a169f4d767fdc9845076f6239f5e
Yoimer <ydrf19@gmail.com>
2019-07-08 10:00:56 -0430

    added update-certification-date-on-equipment (equivalent code on mde expiration date field)

---
+++ triggers/test/equipment/update-certification-date-on-equipment.js
@@ -0,0 +1,31 @@
+var startDate = new Date("{!Start_Date}");
+var validityTime = parseInt("{!Validity_Time}", 10);
+var validityUnit = "{!Validity_Time_Unit#code}";
+
+var getProductFromUnit = function(unit){
+  var seg = 1000,
+    min = seg*60,
+    hr = min*60,
+    day = hr*24,
+    month = day*30,
+    year = day*365;
+  switch(unit){
+    case "D":
+      return day;
+    case "M":
+      return month;
+    case "Y":
+      return year;
+    default:
+      return 1;
+  }
+};
+
+
+var expirationDate = new Date(
+  startDate.getTime()+(validityTime*getProductFromUnit(validityUnit))
+  );
+
+rbv_api.println("Expiration Date: "+expirationDate);
+
+expirationDate;
