e3a786043804c32116ccaccf479e45f90c54c43f
Yoimer <ydrf19@gmail.com>
2019-07-08 12:41:53 -0430

    updates create-new-record-api-contract-sql-server. It creates record in SQL server :) \0/

--- gian-franco-originals/api-contracts/create-new-record-api-contract-sql-server.js
+++ gian-franco-originals/api-contracts/create-new-record-api-contract-sql-server.js
@@ -11,5 +11,23 @@ var customerName = "{!Customer_Name}";
 var customerName_validated = rbv_api.selectQuery("SELECT clientcode_ FROM dim_ApiCustomer_t WHERE clientname_#value='" + customerName + "'", 1);
 
 if (customerName_validated.length > 0) {
-    rbv_api.println("----- " + customerName_validated.length)
+    rbv_api.println("----- " + customerName_validated.length);
+
+    arr["name"] = "{!name#value}";
+    arr["clientcode_"] = customerName_validated[0][0];
+    rbv_api.println("clientcode_: " + arr["clientcode_"]);
+    arr["contractcode_"] = "{!Contract_ID_No_Hyperlink}";
+    arr["contractdescr_"] = "{!Description}";
+    arr["contractname_"] = "{!Contract_Name}";
+    arr["datetransfer_"] = ActualDate;
+    arr["id_"] = "{!id}";
+    arr["operation_"] = "I";
+    arr["sent_"] = "True";
+    arr["validfrom_"] = new Date("{!Starting_Date}");
+    arr["validto_"] = new Date("{!Expiration_Date}");
+    rbv_api.println("create contract");
+    rbv_api.createRecord("dim_APIContracts_t", arr);
+
+} else {
+    return "Customer does not exist in SQL server DB. Please add it first";
 }
